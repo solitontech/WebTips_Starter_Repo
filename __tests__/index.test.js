dc8245d5908d5f5e687829072171ce7573f4c2e4
Reena Sajad <reena.sajad@solitontech.com>
2023-07-19 12:53:57 +0530

    Update index.js and index.js.test files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,6 @@
 // Code to show file structure. Should be changed according to the assignment
-// import { subtract } from "../index";
-
-// test("subtract 2 - 1 to equal 1", () => {
-//   expect(subtract(2, 1)).toBe(1);
-// });
 
 const index = require('../index');
-
 test("subtract 2 - 1 to equal 1", () => {
   expect(index(2, 1)).toBe(1);
 });
--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Sample code that needs to be changed according to the assignment
 
-// export default function subtract(a, b) {
-//   return a - b;
-// };
-
 const index = (a,b) => a - b;
 
-// export default subtract;
 module.exports = index;
