fb115fa860ece5b2a31421780b386935558dce42
Reena Sajad <reena.sajad@solitontech.com>
2023-07-19 11:15:43 +0530

    Update index.js, index.html and index.test.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// Sample code that needs to be changed according to the assignment 
+document.write(5 + 6);
+document.querySelector(".heading").style.fontSize = "23px";
+
+const subtract = (a,b) => a - b;
+
+module.exports = subtract;
