3f02ab0df26ef9df708ede36172c538f8b32bee9
Reena Sajad <127917550+ReenaSajadHyder@users.noreply.github.com>
2023-07-20 18:26:42 +0530

    Delete sample.test.js

--- __tests__/sample.test.js
+++
@@ -1,6 +0,0 @@
-// Code to show file structure. Should be changed according to the assignment
-
-const index = require('../index');
-test("subtract 2 - 1 to equal 1", () => {
-  expect(index(2, 1)).toBe(1);
-});
